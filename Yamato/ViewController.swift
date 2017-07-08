5e25936490cf931d6faecb10013798e1dc64f84f
Raphaël <raphael@Alu.local>
2017-07-08 21:49:16 +0200

    Initial Commit

---
+++ Yamato/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Yamato
+//
+//  Created by Raphaël Calabro on 08/07/2017.
+//  Copyright © 2017 Raphaël Calabro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
