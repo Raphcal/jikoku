1c2f1f505a8ea2a151c664d81ab35489a6debe0b
Raphaël <raphael@Alu.local>
2017-07-08 21:53:39 +0200

    Ajout initial

---
+++ Yamato OSX/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Yamato OSX
+//
+//  Created by Raphaël Calabro on 08/07/2017.
+//  Copyright © 2017 Raphaël Calabro. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Yamato OSX/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Yamato OSX
+//
+//  Created by Raphaël Calabro on 08/07/2017.
+//  Copyright © 2017 Raphaël Calabro. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
