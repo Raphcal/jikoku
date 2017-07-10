514784d3a3ae5c2b87785d644d4c8411d1377406
Raphaël <raphael@Alu.local>
2017-07-10 13:26:04 +0200

    Ajout de Melisse et du contrôleur initial.

---
+++ Yamato/GameViewController.swift
@@ -0,0 +1,13 @@
+//
+//  GameViewController.swift
+//  Yamato
+//
+//  Created by Raphaël Calabro on 10/07/2017.
+//  Copyright © 2017 Raphaël Calabro. All rights reserved.
+//
+
+import Melisse
+
+class GameViewController : MelisseViewController {
+    
+}
