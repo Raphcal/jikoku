236f032861f545d4384829645e43274d5ea48f7b
Raphaël <raphael@Alu.local>
2017-07-19 13:52:13 +0200

    Tentative de création du sprite 私

--- GameScene.swift
+++ GameScene.swift
@@ -10,20 +10,38 @@ import Foundation
 import GLKit
 import Melisse
 
+fileprivate let spriteSize: GLfloat = 48
+
 class GameScene : Scene {
 
     let backgroundColor = Color<GLfloat>(hex: 0xE4C9A0)
     
-    func load() {
+    let spriteFactory: SpriteFactory
+    let player: Sprite
+    var camera: Camera
+    
+    init() {
+        if let atlas = SpriteAtlas(string: "私一二三四五六七八九十日本元気白", size: Int(spriteSize)) {
+            spriteFactory = SpriteFactory(capacity: 1024, spriteAtlas: atlas)
+        } else {
+            print("Atlas creation error")
+            spriteFactory = SpriteFactory()
+        }
         
+        let screenBounds = UIScreen.main.bounds
+        
+        camera = Camera()
+        camera.center(GLfloat(screenBounds.width), height: GLfloat(screenBounds.height))
+        
+        player = spriteFactory.sprite(0, topLeft: Point(x: camera.frame.width / 2 - spriteSize, y: camera.frame.height - spriteSize - 128))
     }
     
     func updateWith(_ timeSinceLastUpdate: TimeInterval) {
-        // TODO: Écrire le code
+        spriteFactory.updateWith(timeSinceLastUpdate)
     }
     
     func draw() {
-        // TODO: Écrire le code
+        spriteFactory.draw(at: camera.frame.topLeft)
     }
 
 }
