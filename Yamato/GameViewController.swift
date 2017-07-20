aeaa0aed3c7d1b2170ad7eeead7b25b93524c41a
Raphaël <raphael@Alu.local>
2017-07-20 17:02:24 +0200

    Gestion basique du déplacement du joueur.

--- Yamato/GameViewController.swift
+++ Yamato/GameViewController.swift
@@ -6,12 +6,31 @@
 //  Copyright © 2017 Raphaël Calabro. All rights reserved.
 //
 
+import UIKit
 import Melisse
 
 class GameViewController : MelisseViewController {
     
+    var panOrigin = Point<GLfloat>()
+    
     override func initialScene() -> Scene {
         return GameScene()
+    }
+    
+    @IBAction func handlePan(from recognizer: UIPanGestureRecognizer) {
+        guard let gameScene = GameScene.current else {
+            return
+        }
+        
+        let player = gameScene.player
+        
+        if recognizer.state == .began {
+            panOrigin = player.frame.center
+        }
+        
+        let translation = recognizer.translation(in: self.view)
+        
+        player.frame.center = Point(x: panOrigin.x + GLfloat(translation.x), y: panOrigin.y + GLfloat(translation.y))
     }
     
 }
