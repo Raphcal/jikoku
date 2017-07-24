9c33510b26486ba5d21fe3bef6442601e3415410
Raphaël <raphael@Alu.local>
2017-07-24 18:55:35 +0200

    Début d'une classe de gestion des niveaux.

---
+++ LevelManager.swift
@@ -0,0 +1,23 @@
+//
+//  LevelManager.swift
+//  Yamato
+//
+//  Created by Raphaël Calabro on 24/07/2017.
+//  Copyright © 2017 Raphaël Calabro. All rights reserved.
+//
+
+import Foundation
+
+class LevelManager {
+    
+    let level: Level
+    
+    init(level: Level) {
+        self.level = level
+    }
+    
+    func update(with timeSinceLastUpdate: TimeInterval) {
+        // TODO: Gérer le niveau
+    }
+    
+}
