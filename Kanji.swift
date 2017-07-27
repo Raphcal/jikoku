85f16a86131882f49defc891bed6a9ac9d4c32c9
Raphaël <raphael@Alu.local>
2017-07-27 09:23:54 +0200

    Début d'un structure pour les kanjis.

---
+++ Kanji.swift
@@ -0,0 +1,16 @@
+//
+//  Kanji.swift
+//  Yamato
+//
+//  Created by Raphaël Calabro on 27/07/2017.
+//  Copyright © 2017 Raphaël Calabro. All rights reserved.
+//
+
+import Foundation
+
+struct Kanji {
+    var character: Character
+    var meanings: [String]
+    var kunyomis: [String]
+    var onyomis: [String]
+}
