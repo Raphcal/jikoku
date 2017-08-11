c66ffd10f3474cf4b7d245c5a7071828815834f7
Raphaël <raphael@Alu.local>
2017-08-11 11:48:19 +0200

    Suppression du forçage à 16x16 des tirs.

--- StraightShootingStyle.swift
+++ StraightShootingStyle.swift
@@ -52,7 +52,7 @@ class StraightShootingStyle : ShootingStyle {
             let speed = Point<GLfloat>(x: cosf(angle) * definition.shotSpeed,
                                        y: sinf(angle) * definition.shotSpeed)
             let shot = spriteFactory.sprite(spriteDefinition)
-            shot.frame = Rectangle(x: left, y: point.y, width: 16, height: 16)
+            shot.frame.center = Point(x: left, y: point.y)
             shot.motion = ShotMotion(speed: speed)
             shot.hitbox = CenteredSpriteHitbox(sprite: shot, size: Size(width: shot.frame.width * 0.6666, height: shot.frame.height * 0.6666))
             
